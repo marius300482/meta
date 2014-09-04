dd6cb8b02c3b9dd0589242a5cad92733d6ba7676
Denny Kunschmann <dku@outermedia.de>
2014-09-04 16:20:11 +0200

    Piwik-Integration per ViewHelper

---
+++ meta/vufind/module/Ida/src/Ida/View/Helper/Genderbib/Factory.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Factory for Genderbib view helpers.
+ *
+ * @package  View_Helpers
+ * @author   dkuom <dku@outermedia.de>
+ */
+namespace Ida\View\Helper\Genderbib;
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * Factory for Genderbib view helpers.
+ *
+ * @package  View_Helpers
+ * @author   dkuom <dku@outermedia.de>
+ */
+class Factory {
+
+    /**
+     * Construct the Piwik Analytics helper.
+     *
+     * @param ServiceManager $sm Service manager.
+     *
+     * @return PiwikAnalytics
+     */
+    public static function getPiwikAnalytics(ServiceManager $sm) {
+
+        // Read config file
+        $config = $sm->getServiceLocator()->get('VuFind\Config')->get('config');
+
+        $trackerURL = $siteId = null;
+
+        // Piwik config exists?
+        if (isset($config->Piwik->trackerURL)) {
+            $trackerURL = $config->Piwik->trackerURL;
+        }
+
+        if (isset($config->Piwik->siteId)) {
+            $siteId = $config->Piwik->siteId;
+        }
+
+        return new PiwikAnalytics($trackerURL, $siteId);
+    }
+}
