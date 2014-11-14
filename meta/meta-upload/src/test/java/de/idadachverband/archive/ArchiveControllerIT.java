629912b17abe0a043b6504044fd23cb67d0d207f
chboehm <cbo@outermedia.de>
2014-11-14 17:28:40 +0100

    Add missing import

--- meta/meta-upload/src/test/java/de/idadachverband/archive/ArchiveControllerIT.java
+++ meta/meta-upload/src/test/java/de/idadachverband/archive/ArchiveControllerIT.java
@@ -12,6 +12,7 @@ import org.testng.annotations.Test;
 
 import javax.inject.Inject;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
