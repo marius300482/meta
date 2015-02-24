54a01688745c4fcc41029ac751ae6e87c9366a26
Patrick Heiß <phe@outermedia.de>
2015-02-24 15:42:34 +0100

    Implemented: https://github.com/marius300482/meta/issues/56

--- meta/vufind/themes/meta-bootstrap3/js/meta.js
+++ meta/vufind/themes/meta-bootstrap3/js/meta.js
@@ -3,6 +3,13 @@ $(function(){
     moreFacets('institution');
 });
 
+// Register "closer" element to clear search fields
+$(window).load(function() {
+    $(".closer").click(function () {
+        $(this).parent().find("input:visible").first().val("");
+    });
+});
+
 // Patching the typeahead library. Submitting the form after selection
 $(window).load(function() {
     $('.autocomplete').bind('typeahead:selected', function(obj) {
