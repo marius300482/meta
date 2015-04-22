26a08ba639cafa6f808db31d8401d5b59ef14b23
Patrick Heiß <phe@outermedia.de>
2015-04-22 13:23:33 +0200

    Continued implementing institution list.

--- meta/vufind/module/Ida/src/Ida/Controller/TopicsController.php
+++ meta/vufind/module/Ida/src/Ida/Controller/TopicsController.php
@@ -72,7 +72,7 @@ class TopicsController extends BrowseController
         $view->institutions = $this->getInstitutions();
         $view->inventoryFacet = $this->getInventoryfacet();
         $view->solrDriver = new SolrDefault();
-        $view->randomBooks = $this->getRandomBooks();
+        $view->randomBooks = $this->getRandomItems();
         return $view;
     }
 
@@ -119,7 +119,7 @@ class TopicsController extends BrowseController
         return $facetContent;
     }
 
-    protected function getRandomBooks()
+    protected function getRandomItems()
     {
         $idSourceFile = APPLICATION_PATH . "/module/Ida/data/randomItems.ini";
         $maxItems = 3;
@@ -148,7 +148,60 @@ class TopicsController extends BrowseController
 
     protected function getInstitutions()
     {
-        require_once APPLICATION_PATH . "/module/Ida/data/institutionList.php";
+        $institutionList = array(
+            "Germany" => array(
+                "Alice Salomon Archiv der ASH Berlin",
+                "Archiv der deutschen Frauenbewegung",
+                "Archiv des Lette-Vereins",
+                "Archiv Frau und Musik",
+                "Archiv zur Geschichte von Tempelhof und Schöneberg in Berlin",
+                "ausZeiten, Bildung, Information, Forschung und Kommunikation für Frauen e.V.",
+                "BAF e.V., Bildungszentrum und Archiv zur Frauengeschichte Baden-Württembergs",
+                "belladonna, Kultur, Bildung und Wirtschaft für Frauen e.V.",
+                "D.I.W.A. - FrauenForschungsStelle Münster e.V.",
+                "DENKtRÄUME - hamburger frauenbibliothek in Kooperation mit Landesfrauenrat Hamburg e.V.",
+                "Feministisches Archiv Marburg",
+                "Frauen-Kultur-Archiv/ Genderforschungs-Transferstelle",
+                "Frauenarchiv Dortmund - Universitätsbibliothek Dortmund",
+                "Frauenbibliothek des Autonomen Frauenreferats",
+                "Frauenforschungs-, -bildungs- und -informationszentrum FFBIZ e.V.",
+                "FrauenGenderBibliothek Saar",
+                "FrauenMediaTurm, Das Archiv und Dokumentationszentrum",
+                "FrauenStadtArchiv Dresden / FrauenBildungsHaus Dresden e.V.",
+                "Genderbibliothek am Zentrum für transdisziplinäre Geschlechterstudien",
+                "GrauZone, Dokumentationsstelle zur nichtstaatlichen Frauenbewegung in der DDR",
+                "Helene-Lange-Archiv",
+                "Kölner Frauengeschichtsverein e.V.",
+                "Lila ArchivZ e.V.",
+                "LLL e.V. Lesbenarchiv Frankfurt am Main",
+                "Louise-Otto-Peters-Archiv",
+                "Madonna-Archiv des Vereins Madonna e.V. – Verein zur Förderung der beruflichen und kulturellen Bildung von Sexarbeiterinnen",
+                "MONAliesA Leipzig Feministische Bibliothek",
+                "Spinnboden Lesbenarchiv und Bibliothek e.V.",
+                "Studentische Frauenbibliothek Lieselle",
+                "Terre des Femmes Dokumentationsstelle",
+                "Zentrum GenderWissen - Zentrale Bibliothek Frauenforschung & Gender Studies Hamburg"
+            ),
+            "Italy" => array(
+                "Frauenarchiv Bozen - Archivio storico delle donne"
+            ),
+            "Luxembourg" => array(
+                "Cid | Fraen an Gender"
+            ),
+            "Austria" => array(
+                "Frauen*solidarität feministisch-entwicklungspolitische Informations- und Bildungsarbeit",
+                "im C3 – Centrum für Internationale Entwicklung",
+                "FRIDA - Verein zur Förderung und Vernetzung frauenspezifischer Informations- und Dokumentationseinrichtungen in Österreich",
+                "Öffentliche Frauenbibliothek AEP",
+                "Sammlung Frauennachlässe am Institut für Geschichte der Universität Wien",
+                "STICHWORT, Archiv der Frauen- und Lesbenbewegung, Bibliothek · Dokumentation · Multimedia"
+            ),
+            "Swiss" => array(
+                "frauen_bibliothek basel",
+                "schema f"
+            )
+        );
+
         return $institutionList;
     }
 
