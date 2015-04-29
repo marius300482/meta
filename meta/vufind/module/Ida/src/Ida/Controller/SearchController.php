d0d1910edf53bd64cc14154f16b83524f2fc93b4
Patrick Heiß <phe@outermedia.de>
2015-04-29 11:16:57 +0200

    Continued implementing #97

--- meta/vufind/module/Ida/src/Ida/Controller/SearchController.php
+++ meta/vufind/module/Ida/src/Ida/Controller/SearchController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Ida\Controller;
 
+use Zend\Stdlib\Parameters;
+
 /**
  * Class IDA SearchController
  *
@@ -57,6 +59,18 @@ class SearchController extends \VuFind\Controller\SearchController
      */
     public function resultsAction()
     {
+        // PHE START: Empty search is forbidden #71
+        if ($this->isEmptySearch()) {
+            return $this->forwardTo('Error', 'Search');
+        }
+        // PHE END: Empty search is forbidden #71
+
+        // Default case -- standard behavior.
+        return parent::resultsAction();
+    }
+
+    public function contributorsAction()
+    {
         // Special case -- redirect tag searches.
         $tag = $this->params()->fromQuery('tag');
         if (!empty($tag)) {
@@ -75,35 +89,110 @@ class SearchController extends \VuFind\Controller\SearchController
             return $this->forwardTo('Tag', 'Home');
         }
 
-        // Default case -- standard behavior.
-        return parent::resultsAction();
-    }
+        $view = $this->createViewModel();
 
-    public function contributorsAction()
-    {
-        // Get result view for side facet
-        $resultView = $this->resultsAction();
+        // Handle saved search requests:
+        $savedId = $this->params()->fromQuery('saved', false);
+        if ($savedId !== false) {
+            return $this->redirectToSavedSearch($savedId);
+        }
+
+        $results = $this->getResultsManager()->get($this->searchClassId);
+        $params = $results->getParams();
+        $params->setFacetLimit(999999); // PHE change the limit
+
+        // Enable recommendations unless explicitly told to disable them:
+        $noRecommend = $this->params()->fromQuery('noRecommend', false);
+        $params->recommendationsEnabled(!$noRecommend);
+
+        // Send both GET and POST variables to search class:
+        $params->initFromRequest(
+            new Parameters(
+                $this->getRequest()->getQuery()->toArray()
+                + $this->getRequest()->getPost()->toArray()
+            )
+        );
+
+        // Make parameters available to the view:
+        $view->params = $params;
+
+        // Attempt to perform the search; if there is a problem, inspect any Solr
+        // exceptions to see if we should communicate to the user about them.
+        try {
+            // Explicitly execute search within controller -- this allows us to
+            // catch exceptions more reliably:
+            $results->performAndProcessSearch();
+
+            // If a "jumpto" parameter is set, deal with that now:
+            if ($jump = $this->processJumpTo($results)) {
+                return $jump;
+            }
+
+            // Send results to the view and remember the current URL as the last
+            // search.
+            $view->results = $results;
+            $this->rememberSearch($results);
+
+            // Add to search history:
+            if ($this->saveToHistory) {
+                $user = $this->getUser();
+                $sessId = $this->getServiceLocator()->get('VuFind\SessionManager')
+                    ->getId();
+                $history = $this->getTable('Search');
+                $history->saveSearch(
+                    $this->getResultsManager(), $results, $sessId,
+                    $history->getSearches(
+                        $sessId, isset($user->id) ? $user->id : null
+                    )
+                );
+            }
+
+            // Set up results scroller:
+            if ($this->resultScrollerActive()) {
+                $this->resultScroller()->init($results);
+            }
+        } catch (\VuFindSearch\Backend\Exception\BackendException $e) {
+            if ($e->hasTag('VuFind\Search\ParserError')) {
+                // If it's a parse error or the user specified an invalid field, we
+                // should display an appropriate message:
+                $view->parseError = true;
+
+                // We need to create and process an "empty results" object to
+                // ensure that recommendation modules and templates behave
+                // properly when displaying the error message.
+                $view->results = $this->getResultsManager()->get('EmptySet');
+                $view->results->setParams($params);
+                $view->results->performAndProcessSearch();
+            } else {
+                throw $e;
+            }
+        }
+        // Save statistics:
+        if ($this->logStatistics) {
+            $this->getServiceLocator()->get('VuFind\SearchStats')
+                ->log($results, $this->getRequest());
+        }
 
         // Get contributor facet
-        $facets = $resultView->results->getfacetList();
-        $resultView->contributorFacetKey = 'contributor_facet';
+        $facets = $view->results->getfacetList();
+        $view->contributorFacetKey = 'contributor_facet';
 
-        if (!isset($facets[$resultView->contributorFacetKey])) {
-            throw new \Exception('Facet "' . $resultView->contributorFacetKey . '" does not exist!');
+        if (!isset($facets[$view->contributorFacetKey])) {
+            throw new \Exception('Facet "' . $view->contributorFacetKey . '" does not exist!');
         } else {
-            $resultView->contributorFacet = $facets[$resultView->contributorFacetKey];
+            $view->contributorFacet = $facets[$view->contributorFacetKey];
         }
 
         // Set up paginator
-        $adapter = new \Zend\Paginator\Adapter\ArrayAdapter($resultView->contributorFacet['list']);
-        $pageLimit = max($resultView->results->getParams()->getLimit(), 20);
+        $adapter = new \Zend\Paginator\Adapter\ArrayAdapter($view->contributorFacet['list']);
+        $pageLimit = max($view->results->getParams()->getLimit(), 20);
         $paginator = new \Zend\Paginator\Paginator($adapter);
-        $paginator->setCurrentPageNumber($resultView->results->getParams()->getPage())
+        $paginator->setCurrentPageNumber($view->results->getParams()->getPage())
             ->setItemCountPerPage($pageLimit)
             ->setPageRange(5);
-        $resultView->paginator = $paginator;
-        $resultView->pages = $paginator->getPages();
+        $view->paginator = $paginator;
+        $view->pages = $paginator->getPages();
 
-        return $resultView;
+        return $view;
     }
 }
