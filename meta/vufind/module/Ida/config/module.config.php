38add63ac1886e018c39de9816c8da417e89bdda
Robert <rpr@outermedia.de>
2015-07-09 10:40:04 +0200

    Revert "Grouped search results"
    This reverts commit 65b12990f256adcc5659c4f8a9f1bdcfd785a8ae.

--- meta/vufind/module/Ida/config/module.config.php
+++ meta/vufind/module/Ida/config/module.config.php
@@ -24,11 +24,6 @@ $config = array(
                     'jstree' => 'Ida\Hierarchy\TreeRenderer\JSTree',
                 )
             ),
-            'search_backend' => array(
-                'factories' => array(
-                    'Solr' => 'Ida\Search\Factory\SolrDefaultBackendFactory',
-                )
-            ),
         ),
         'recorddriver_tabs' => array(
             // Disable Holdings, Comments and  Staff View
