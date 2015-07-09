ca3c3ad734d0d83436765a3c14d5e9cca2deda22
Robert <rpr@outermedia.de>
2015-07-09 10:39:53 +0200

    Revert "Grouped search results"
    This reverts commit acc665ed9df63b8219fd52f53678994c901451c3.

--- meta/vufind/module/Ida/src/Ida/Backend/Solr/Response/Json/RecordCollection.php
+++ meta/vufind/module/Ida/src/Ida/Backend/Solr/Response/Json/RecordCollection.php
@@ -3,21 +3,86 @@
 /**
  * Simple JSON-based record collection.
  *
- * @category Ida
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
  * @package  Search
- * @author   <dku@outermedia.de>
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
  */
 
 namespace Ida\Backend\Solr\Response\Json;
 
-class RecordCollection extends \VuFindSearch\Backend\Solr\Response\Json\RecordCollection {
+use VuFindSearch\Response\AbstractRecordCollection;
+
+/**
+ * Simple JSON-based record collection.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class RecordCollection extends \VuFindSearch\Backend\Solr\Response\Json\RecordCollection
+//class RecordCollection extends AbstractRecordCollection
+{
+    /**
+     * Template of deserialized SOLR response.
+     *
+     * @see self::__construct()
+     *
+     * @var array
+     */
+    protected static $template = array(
+        'responseHeader' => array(),
+        'grouped' => array(),
+        'spellcheck'     => array('suggestions' => array()),
+        'facet_counts'   => array(),
+    );
+//    protected static $template = array(
+//        'responseHeader' => array(),
+//        'response'       => array('numFound' => 0, 'start' => 0),
+//        'spellcheck'     => array('suggestions' => array()),
+//        'facet_counts'   => array(),
+//    );
 
     /**
-     * Grouping field name if exists.
+     * Deserialized SOLR response.
      *
-     * @var string
+     * @var array
      */
-    protected $groupFieldName;
+//    protected $response;
+
+    /**
+     * Facets.
+     *
+     * @var Facets
+     */
+//    protected $facets;
+
+    /**
+     * Spellcheck information.
+     *
+     * @var Spellcheck
+     */
+//    protected $spellcheck;
 
     /**
      * Constructor.
@@ -26,41 +91,78 @@ class RecordCollection extends \VuFindSearch\Backend\Solr\Response\Json\RecordCo
      *
      * @return void
      */
-    public function __construct(array $response) {
-
+    public function __construct(array $response)
+    {
         $this->response = array_replace_recursive(static::$template, $response);
-
-        if (true === $this->isGrouped()) {
-
-            // Extract grouping field name
-            $this->groupFieldName = reset(array_keys($this->getGroups()));
-
-            $this->offset = 0; // TODO: No "start" info provided
-        }
-        else {
-
-            $this->offset = $this->response['response']['start'];
-        }
-
+        $this->offset = 0; // todo
+//        $this->offset = $this->response['response']['start'];
         $this->rewind();
     }
 
-    protected function isGrouped() {
-
-        $groups = $this->getGroups();
-
-        return 0 < count($groups);
-    }
+    /**
+     * Return spellcheck information.
+     *
+     * @return Spellcheck
+     */
+    /*public function getSpellcheck()
+    {
+        if (!$this->spellcheck) {
+            $params = isset($this->response['responseHeader']['params'])
+                ? $this->response['responseHeader']['params'] : array();
+            $sq = isset($params['spellcheck.q'])
+                ? $params['spellcheck.q']
+                : (isset($params['q']) ? $params['q'] : '');
+            $sugg = isset($this->response['spellcheck']['suggestions'])
+                ? $this->response['spellcheck']['suggestions'] : array();
+            $this->spellcheck = new Spellcheck($sugg, $sq);
+        }
+        return $this->spellcheck;
+    }*/
 
     /**
      * Return total number of records found.
      *
      * @return int
      */
-    public function getTotal() {
-
-        return true === $this->isGrouped()
-            ? $this->response['grouped'][$this->groupFieldName]['matches']
-            : $this->response['response']['numFound'];
+    public function getTotal()
+    {
+        // todo
+        return $this->response['grouped']['groupID']['matches'];
+//        return $this->response['response']['numFound'];
     }
+
+    /**
+     * Return SOLR facet information.
+     *
+     * @return array
+     */
+    /*public function getFacets()
+    {
+        if (!$this->facets) {
+            $this->facets = new Facets($this->response['facet_counts']);
+        }
+        return $this->facets;
+    }*/
+
+    /**
+     * Get grouped results.
+     *
+     * @return array
+     */
+    /*public function getGroups()
+    {
+        return isset($this->response['grouped'])
+            ? $this->response['grouped'] : array();
+    }*/
+
+    /**
+     * Get highlighting details.
+     *
+     * @return array
+     */
+    /*public function getHighlighting()
+    {
+        return isset($this->response['highlighting'])
+            ? $this->response['highlighting'] : array();
+    }*/
 }
